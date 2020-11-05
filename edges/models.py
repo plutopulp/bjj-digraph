4a9ab826f9c9d120ce7eeeefa236f86214693749
mobiustrip <yvan@metatech.ie>
2020-11-05 19:14:44 +0100

    added edge model

---
+++ edges/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from nodes.models import Node
+
+
+class Edge(models.Model):
+    """ A class to represent the edges of a bjj digraph """
+
+    source_node = models.ForeignKey(
+        Node, on_delete=models.CASCADE, related_name="output_edges"
+    )
+    target_node = models.ForeignKey(
+        Node, on_delete=models.CASCADE, related_name="input_edges"
+    )
+    # The rationale behind transitioning between nodes
+    rationale = models.TextField(default="", blank=True)
+
+    class Meta:
+        verbose_name = "Edge"
+        verbose_name_plural = "Edges"
+
+    def __str__(self):
+        return f"{self.source_node.title} - {self.target_node.title}"
--- nodes/models.py
+++ nodes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 NODE_TYPES = (
     ("position", "position"),
@@ -14,9 +15,19 @@ class Node(models.Model):
     """ A class to represent digraph nodes """
 
     title = models.CharField(max_length=200)
-    node_type = models.CharField(choices=NODE_TYPES, default="")
-    description = models.TextField(default="")
-    comment = models.TextField(default="")
+    created_at = models.DateTimeField(default=timezone.now)
+    node_type = models.CharField(choices=NODE_TYPES, default="", max_length=50)
+    description = models.TextField(default="", blank=True)
+    comment = models.TextField(default="", blank=True)
     effectiveness = models.IntegerField(default=50)
     priority = models.IntegerField(default=50)
     proficiency = models.IntegerField(default=50)
+    position_x = models.FloatField(default=100)
+    position_y = models.FloatField(default=100)
+
+    class Meta:
+        verbose_name = "Node"
+        verbose_name_plural = "Nodes"
+
+    def __str__(self):
+        return self.title
