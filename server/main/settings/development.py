a061202e5282f60be13d7f5a644493320372e65d
mobiustrip <yvan@metatech.ie>
2021-02-09 18:14:43 +0100

    added allowed host

--- server/main/settings/development.py
+++ server/main/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "68.183.6.51"]
 
 DATABASES = {
     "default": {
