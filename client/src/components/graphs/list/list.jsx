a3fff5e2173f11a54785bab06871ae8101443e55
mobiustrip <yvan@metatech.ie>
2021-02-18 20:34:13 +0100

    fixed double navbar bug

--- client/src/components/graphs/list/list.jsx
+++ client/src/components/graphs/list/list.jsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { GraphsContext } from "../../../contexts/graphs";
 import Card from "./card/card";
 import { Card as UICard, Container } from "semantic-ui-react";
-import AppNavbar from "../../navbars/app/navbar";
 
 const StyledContainer = styled(Container)`
   margin-top: 10em;
@@ -14,23 +13,20 @@ const GraphCardList = () => {
   const { graphs } = React.useContext(GraphsContext);
 
   return (
-    <React.Fragment>
-      <AppNavbar fixed={true} />
-      <StyledContainer>
-        <UICard.Group>
-          {graphs.map((graph) => (
-            <Card
-              key={graph.id}
-              title={graph.title}
-              createdAt={graph.createdAt}
-              description={graph.description}
-              id={graph.id}
-              slug={graph.slug}
-            />
-          ))}
-        </UICard.Group>
-      </StyledContainer>
-    </React.Fragment>
+    <StyledContainer>
+      <UICard.Group>
+        {graphs.map((graph) => (
+          <Card
+            key={graph.id}
+            title={graph.title}
+            createdAt={graph.createdAt}
+            description={graph.description}
+            id={graph.id}
+            slug={graph.slug}
+          />
+        ))}
+      </UICard.Group>
+    </StyledContainer>
   );
 };
 
